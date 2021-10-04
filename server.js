98f33c68a962fb9adf61d5a3313c1f34f7f2eb68
JD Rowley <jdrowley1989@gmail.com>
2021-10-04 15:47:50 -0600

    connect server to database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,21 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Sh4tteredgrave',
+        database: 'election'
+    },
+    console.log('Connected to election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // default for any other response
 app.use((req, res) => {
