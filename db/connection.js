899d0026e4b22fcf60b7c433cd23de201ab4f1d3
JD Rowley <jdrowley1989@gmail.com>
2021-10-07 21:08:32 -0600

    add new password

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'Sh4tteredgrave',
+        password: 'pheonix1',
         database: 'election'
     }
 );
